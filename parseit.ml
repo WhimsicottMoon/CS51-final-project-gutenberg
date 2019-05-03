78bd16661201db916fdd10339e4b5f898b0f796d
Veronica Tang <vtang@college.harvard.edu>
2019-05-03 09:39:54 -0700

    Can parse strings correctly!

--- parseit.ml
+++ parseit.ml
@@ -1,3 +1,15 @@
 (*Just the portion that parses the input strings*)
 
-let text = "he saw he was a saw" ;;
+let text = "He Sa!w H;e was sAw.\n" ;;
+
+let punctuation = ['.'; '!'; '"'; ':'; ';'; ','; '-'; '\n'] ;;
+
+(*deletes all punctuation by splitting on a character and concatenating once more
+for each character in punctuation*)
+let strip_punctuation (str : string) : string =
+  List.fold_right (fun c s -> String.concat  "" (String.split_on_char c s)) punctuation str;;
+
+let process (s : string) : string list = 
+  String.split_on_char ' ' (String.lowercase_ascii (strip_punctuation s)) ;;
+
+List.iter (fun s -> Printf.printf "%s\n" s) (process text) ;;
