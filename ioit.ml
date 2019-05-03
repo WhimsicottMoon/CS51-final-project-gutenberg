06faa2679c4eb3745af73980ac31de9ddde5c26b
A Alias <anagadinesh@college.harvard.edu>
2019-05-03 14:18:23 -0700

    comented io

--- ioit.ml
+++ ioit.ml
@@ -1,7 +1,9 @@
-open Printf
-let all_word_lists = ref []
-let file_of_files = "masterfile.txt"
+let all_word_lists = ref [] (* is a string list list with inner lists as books  split into words*)
+let file_of_files = "masterfile.txt" (* has addresses of every book to run this on *)
 
+(* for each book, reads in character by character and splits into
+   new word when hits space, otherwise adds to current word
+ *)
 let make_string (s : string) : string list =
   let next_file_channel = open_in s in
   let word_list = ref [] in
@@ -14,6 +16,7 @@ let make_string (s : string) : string list =
     done; !next_word :: !word_list;
    with End_of_file -> close_in next_file_channel; !next_word :: !word_list ;;
 
+(*goes through the master file and gives the address of each book to make_string*)
 let () =
   let ic = open_in file_of_files in
   try
