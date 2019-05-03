c6e13d2f96c84b9d5dbdaf781c554484f39745f1
Veronica Tang <vtang@college.harvard.edu>
2019-05-03 11:15:08 -0700

    started working on Hashtbl implementation

--- parseit.ml
+++ parseit.ml
@@ -1,8 +1,8 @@
 (*Just the portion that parses the input strings*)
 
-let text = "He Sa!w H;e was sAw.\n" ;;
+let text = "He Saw H;e was sAw.\n" ;;
 
-let punctuation = ['.'; '!'; '"'; ':'; ';'; ','; '-'; '\n'] ;;
+let punctuation = ['.'; '!'; '"'; ':'; ';'; ','; '-'; '\n'] ;; (*TO DO: more punctuation*)
 
 (*deletes all punctuation by splitting on a character and concatenating once more
 for each character in punctuation*)
@@ -11,5 +11,9 @@ let strip_punctuation (str : string) : string =
 
 let process (s : string) : string list = 
   String.split_on_char ' ' (String.lowercase_ascii (strip_punctuation s)) ;;
+
+(*let count (lst : string list) : (string, int) Hashtbl.t =
+  let frequencies = Hashtbl.create 3 in
+  List.iter (fun s -> if Hashtbl.mem frequencies s then Hashtbl.add frequencies 1 else Hashtbl.find) *)
 
 List.iter (fun s -> Printf.printf "%s\n" s) (process text) ;;
