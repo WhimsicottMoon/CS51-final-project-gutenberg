de3480844b6fcaafbcf632be5d83826420494f6e
Veronica Tang <vtang@college.harvard.edu>
2019-05-03 14:06:58 -0700

    Implemented hashtable storage

--- parseit.ml
+++ parseit.ml
@@ -5,8 +5,8 @@ let split_text = ["He"; "Saw"; "H;e"; "was"; "sAw"; ".\n"] ;;
 
 let punctuation = ['.'; '!'; '"'; ':'; ';'; ','; '-'; '\n'] ;; (*TO DO: more punctuation*)
 
-(*deletes all punctuation by splitting on a character and concatenating once more
-for each character in punctuation*)
+(*deletes all punctuation/other whitespace by splitting on specificed characters
+and concatenating once more*)
 let strip_punctuation (str : string) : string =
   List.fold_right (fun c s -> String.concat  "" (String.split_on_char c s)) punctuation str;;
 
@@ -16,10 +16,13 @@ let process (s : string) : string =
 let rec process_all (lst : string list) : string list =
   match lst with
   | [] -> []
-  | hd :: tl -> (process hd) :: process_all tl ;; (*TO DO: can this be made nicer with a fold*)
+  | hd :: tl -> (process hd) :: process_all tl ;; (*TO DO: can this be made nicer with a fold?*)
 
-(*let count (lst : string list) : (string, int) Hashtbl.t =
+let count (lst : string list) : (string, int) Hashtbl.t =
   let frequencies = Hashtbl.create 3 in
-  List.iter (fun s -> if Hashtbl.mem frequencies s then Hashtbl.add frequencies 1 else Hashtbl.find) *)
+  let add (s : string) : unit =
+    if Hashtbl.mem frequencies s then Hashtbl.replace frequencies s (succ (Hashtbl.find frequencies s))
+    else Hashtbl.add frequencies s 1 in
+  List.iter add lst; frequencies ;;
 
 List.iter (fun s -> Printf.printf "%s\n" s) (process_all split_text) ;;
