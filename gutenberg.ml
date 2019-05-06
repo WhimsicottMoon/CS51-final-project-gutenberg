09d4c41259c3036e34d014f778ae325221aa7c2d
Veronica Tang <vtang@college.harvard.edu>
2019-05-06 00:31:05 -0700

    Ran for NOnfiction

--- gutenberg.ml
+++ gutenberg.ml
@@ -6,7 +6,7 @@ open Plplot
 let all_word_lists = ref []
 
 (* A text file with all the file paths to each book's .txt file *)
-let file_of_files = "Science Fiction.txt"
+let file_of_files = "Nonfiction.txt"
 
 (* List of all the books' file paths *)
 let book_files = ref []
